f6b89fe0d904cfef616ffc18e662f7ec994f8122
Dejan <dejannrt411@gmail.com>
2017-04-14 18:18:49 +0200

    Creating master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    @yield('title')
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Font Awesome CDN -->
+    <script src="https://use.fontawesome.com/ddc4ec2a27.js"></script>
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="{{url()->to('css/styles.css')}}">
+    @yield('styles')
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+    <!-- jQuery CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <!-- Bootstrap JS CDN -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Custom scripts -->
+    <script src="{{url()->to('js/scripts.js')}}"></script>
+    @yield('scripts')
+</body>
+</html>
