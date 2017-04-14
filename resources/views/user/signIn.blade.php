eaeedf4e8cb278dd1691ed28f479a46e1cc7eb1c
Dejan <dejannrt411@gmail.com>
2017-04-15 00:40:44 +0200

    Creating signIn view and login form

---
+++ resources/views/user/signIn.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('title')
+    Sign In
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <p>Please fill out all fields to login</p>
+            <form action="{{route('postSignIn')}}" method="post">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" value="{{old('email')}}" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" value="{{old('password')}}" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary pull-right">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
