75f80ed58213280c75bea0957ab4607d3742890d
Dejan <dejannrt411@gmail.com>
2017-04-15 01:26:33 +0200

    Creating some route groups to apply prefix and middlewares

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,32 @@ Route::get('/', function () {
     return view('shop.index');
 });
 
-Route::get('/signup',[
-    'uses' => 'UserController@getSignUp',
-    'as'  => 'getSignUp'
-]);
+Route::group(['prefix' => 'user'],function(){
+    Route::group(['middleware' => 'guest'],function(){
+        Route::get('/signup',[
+            'uses' => 'UserController@getSignUp',
+            'as'  => 'getSignUp'
+        ]);
 
-Route::post('/signup',[
-    'uses' => 'UserController@postSignUp',
-    'as'   => 'postSignUp'
-]);
+        Route::post('/signup',[
+            'uses' => 'UserController@postSignUp',
+            'as'   => 'postSignUp'
+        ]);
 
-Route::get('/signin',[
-    'uses' => 'UserController@getSignIn',
-    'as'  => 'getSignIn'
-]);
+        Route::get('/signin',[
+            'uses' => 'UserController@getSignIn',
+            'as'  => 'getSignIn'
+        ]);
 
-Route::post('/signin',[
-    'uses' => 'UserController@postSignIn',
-    'as'   => 'postSignIn'
-]);
-
-Route::get('/logout',[
-    'uses' => 'UserController@logout',
-    'as'   => 'logout'
-]);
+        Route::post('/signin',[
+            'uses' => 'UserController@postSignIn',
+            'as'   => 'postSignIn'
+        ]);
+    });
+    Route::group(['middleware' => 'auth'],function(){
+        Route::get('/logout',[
+            'uses' => 'UserController@logout',
+            'as'   => 'logout'
+        ]);
+    });
+});
