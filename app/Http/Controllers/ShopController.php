76bf88ecd66d732e37350e907bb5bb98d9500b71
Dejan <dejannrt411@gmail.com>
2017-04-15 01:36:02 +0200

    Creating ShopController and getIndex method

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    public function getIndex()
+    {
+        return view('shop.index');
+    }
+}
