23c7abe865d580924ac75a65308e5778d41d4e96
Dejan <dejannrt411@gmail.com>
2017-04-14 23:57:15 +0200

    Adding postSignUp method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function getSignUp()
     {
         return view('user.signUp');
+    }
+
+    public function postSignUp(Request $request)
+    {
+        $this->validate($request,[
+            'username' => 'required|min:5|max:20',
+            'name'     => 'required|min:5|max:50',
+            'password' => 'required|min:6',
+            'email'    => 'email|unique:users',
+        ]);
+
+        $user = new User($request->all());
+        $user->save();
+        Auth::login($user,true);
+
+        return redirect('/');
     }
 }
