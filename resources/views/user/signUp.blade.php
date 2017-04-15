a674ae1e784c8a8769315f36e5c4c6adfbac73cb
Dejan <dejannrt411@gmail.com>
2017-04-15 15:00:21 +0200

    Removing value field from password input

--- resources/views/user/signUp.blade.php
+++ resources/views/user/signUp.blade.php
@@ -16,7 +16,7 @@
                 </div>
                 <div class="form-group">
                     <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" value="{{old('password')}}" class="form-control" required>
+                    <input type="password" id="password" name="password" class="form-control" required>
                 </div>
                 <div class="form-group">
                     <label for="name">Name:</label>
