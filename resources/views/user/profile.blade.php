b50ad1a9f7e5f70ba5fc6c2acb06b2680c04bac1
Dejan <dejannrt411@gmail.com>
2017-04-16 01:54:32 +0200

    Adding css class

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2">
-            <p><i>{{auth()->user()->username}}</i> profile</p>
+            <p><i class="search-count">{{auth()->user()->username}}</i> profile</p>
         </div>
     </div>
 @endsection
