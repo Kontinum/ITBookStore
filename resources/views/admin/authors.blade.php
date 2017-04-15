aeb5178fd6b67f66b871eeefd4a9de860a6ba592
Dejan <dejannrt411@gmail.com>
2017-04-15 19:00:05 +0200

    Including searchAuthorsForm partial

--- resources/views/admin/authors.blade.php
+++ resources/views/admin/authors.blade.php
@@ -25,20 +25,7 @@
                     </form>
                 </div>
             </div>
-
-            <div class="panel panel-success">
-                <div class="panel-heading">Search authors</div>
-                <div class="panel-body">
-                    <form action="{{route('searchAuthors')}}" method="get">
-                        <div class="form-group">
-                            <input type="text" name="author-name" value="{{old('author-name')}}" class="form-control" placeholder="Author name" required>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-success pull-right">Search authors</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            @include('partials.searchAuthorsForm')
         </div>
     </div>
 @endsection
