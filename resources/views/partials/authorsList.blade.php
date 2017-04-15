6ae9c1abfa597a75ebe7908ea025670dee89709e
Dejan <dejannrt411@gmail.com>
2017-04-15 19:52:08 +0200

    Creating authorsList partial

---
+++ resources/views/partials/authorsList.blade.php
@@ -0,0 +1,15 @@
+<div class="col-lg-12">
+    <ul class="list-group">
+        @foreach($authors as $author)
+            <li class="list-group-item list-group-item-success col-lg-3 col-md-4 col-sm-6">
+                {{$author->name}}
+                <a href="" class="pull-right" title="Delete author">
+                    <i class="fa fa-trash icon list-icon" aria-hidden="true"></i>
+                </a>
+                <a href="" class="pull-right" title="Edit author">
+                    <i class="fa fa-pencil-square-o icon" aria-hidden="true"></i>
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
