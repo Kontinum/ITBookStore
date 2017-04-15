6c50dabf8fb9e758024850b6ca9e7ac2cd1fb38c
Dejan <dejannrt411@gmail.com>
2017-04-15 14:57:24 +0200

    Adding info-box class

--- resources/views/errors/info-box.blade.php
+++ resources/views/errors/info-box.blade.php
@@ -1,19 +1,19 @@
 <div class="row">
     <div class="col-lg-8 col-lg-offset-2 text-center">
         @if(count($errors) > 0)
-            <div class="alert alert-danger">
+            <div class="info-box alert alert-danger">
                 @foreach($errors->all() as $error)
                     <li>{{$error}}</li>
                 @endforeach
             </div>
         @endif
         @if(session()->has('error'))
-            <div class="alert alert-danger">
+            <div class="info-box alert alert-danger">
                 {{session()->get('error')}}
             </div>
         @endif
         @if(session()->has('success'))
-            <div class="alert alert-success">
+            <div class="info-box alert alert-success">
                 {{session()->get('success')}}
             </div>
         @endif
