904ab35476a5662d831bb7aef8cbbb3d310b253e
Dejan <dejannrt411@gmail.com>
2017-04-15 18:07:01 +0200

    Creating authors view, display authors number and form to add author

---
+++ resources/views/admin/authors.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+
+@section('title')
+    Authors panel
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <p>Authors: <i>{{count($authors)}}</i></p>
+            <hr>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Add new author
+                </div>
+                <div class="panel-body">
+                    <div class="panel-body">
+                        <form action="{{route('addAuthor')}}" method="post">
+                            <div class="form-group">
+                                <input type="text" name="author-name" value="{{old('author-name')}}" class="form-control" placeholder="Author name" required>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn bg-primary pull-right">Add author</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
