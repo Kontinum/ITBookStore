4bf48686e59be483d596ad8528b63a798b17c609
Dejan <dejannrt411@gmail.com>
2017-04-15 15:10:08 +0200

    Adding * for required fields

--- resources/views/user/signUp.blade.php
+++ resources/views/user/signUp.blade.php
@@ -11,28 +11,28 @@
             <form action="{{route('postSignUp')}}" method="post">
                 {{csrf_field()}}
                 <div class="form-group">
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="username" value="{{old('username')}}" class="form-control" required>
+                    <label for="username">Username:<span class="required">*</span></label>
+                    <input type="text" id="username" name="username" value="{{old('username')}}" class="form-control" placeholder="5-20 characters" required>
                 </div>
                 <div class="form-group">
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" class="form-control" required>
+                    <label for="password">Password:<span class="required">*</span></label>
+                    <input type="password" id="password" name="password" class="form-control" placeholder="Minimum 6 characters" required>
                 </div>
                 <div class="form-group">
-                    <label for="name">Name:</label>
-                    <input type="text" id="name" name="name" value="{{old('name')}}" class="form-control" required>
+                    <label for="name">Name:<span class="required">*</span></label>
+                    <input type="text" id="name" name="name" value="{{old('name')}}" class="form-control" placeholder="5-50 characters" required>
                 </div>
                 <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" value="{{old('email')}}" class="form-control" required>
+                    <label for="email">Email:<span class="required">*</span></label>
+                    <input type="email" id="email" name="email" value="{{old('email')}}" class="form-control" placeholder="Valid email address" required>
                 </div>
                 <div class="form-group">
                     <label for="address">Address:</label>
-                    <input type="text" id="address" name="address" value="{{old('address')}}" class="form-control">
+                    <input type="text" id="address" name="address" value="{{old('address')}}" class="form-control" placeholder="Valid address for shipping">
                 </div>
                 <div class="form-group">
                     <label for="phone">Phone:</label>
-                    <input type="text" id="phone" name="phone" value="{{old('phone')}}" class="form-control">
+                    <input type="text" id="phone" name="phone" value="{{old('phone')}}" class="form-control" placeholder="Your phone number">
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary pull-right">Register</button>
