493317f46b4b9ade11c63a5888e32ad5dab7a5c3
Dejan <dejannrt411@gmail.com>
2017-04-15 17:30:47 +0200

    Adding authors method

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    public function authors()
+    {
+        $authors = Author::all();
 
+        return view('admin.authors',['authors' => $authors]);
+    }
 }
