b45ffc94d098b664e9d9e1c2807b29dc541fcc24
Dejan <dejannrt411@gmail.com>
2017-04-17 13:34:33 +0200

    Adding relations in Category and Subcategory model

--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,9 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function subcategories()
+    {
+        return $this->hasMany(Subcategory::class);
+    }
 }
--- app/Subcategory.php
+++ app/Subcategory.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subcategory extends Model
 {
-    //
+    public function categories()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
