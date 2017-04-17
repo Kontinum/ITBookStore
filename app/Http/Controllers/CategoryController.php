4dd88510a5638bf72c46441c908d82faa8420749
Dejan <dejannrt411@gmail.com>
2017-04-17 14:17:19 +0200

    Adding addSubcategory method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Subcategory;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -26,5 +27,22 @@ class CategoryController extends Controller
         $category->save();
 
         return back()->with('success','Category '.$categoryName.' has been successfully added');
+    }
+
+    public function addSubcategory($categoryId, Request $request)
+    {
+        $this->validate($request,[
+            'subcategory-name' => 'required'
+        ]);
+
+        $subcategoryName = $request->input('subcategory-name');
+        $subcategory = new Subcategory([
+            'category_id' => $categoryId,
+            'name'        => $subcategoryName
+        ]);
+        $subcategory->save();
+
+        return back()->with('success','Subcategory '.$subcategoryName.' has been successfully added');
+
     }
 }
