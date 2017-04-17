7ca8ce196584578b930b1b3d80561705eaeb5c77
Dejan <dejannrt411@gmail.com>
2017-04-18 01:00:56 +0200

    Adding jquery code for subcategories toggle

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,13 @@
 $('document').ready(function(){
     //info-box delay and slide
     $('.info-box').delay(3000).slideUp(1000);
+
+    //subcategory toggle
+    var subcategoryList = $('.subcategory-list');
+    var subcategoryToggle = $('.subcategory-toggle');
+
+    subcategoryList.hide();
+    subcategoryToggle.click(function () {
+        $(this).parent().next().slideToggle(700);
+    });
 });
