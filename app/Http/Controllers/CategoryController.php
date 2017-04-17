7f8a1fd527163e1f8b28922ad731f38899ec6cf2
Dejan <dejannrt411@gmail.com>
2017-04-17 13:02:52 +0200

    Adding addCategory method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,4 +12,19 @@ class CategoryController extends Controller
         $categories = Category::all();
         return view('admin.categories', ['categories' => $categories]);
     }
+
+    public function addCategory(Request $request)
+    {
+        $this->validate($request,[
+            'category-name' => 'required'
+        ]);
+
+        $categoryName = $request->input('category-name');
+        $category = new Category([
+            'name' => $categoryName
+        ]);
+        $category->save();
+
+        return back()->with('success','Category '.$categoryName.' has been successfully added');
+    }
 }
