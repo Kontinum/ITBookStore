fcece55e5929673dcc4e866469c4dd98e3f36cca
Dejan <dejannrt411@gmail.com>
2017-04-17 14:16:23 +0200

    Adding fillable fields

--- app/Subcategory.php
+++ app/Subcategory.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subcategory extends Model
 {
+    protected $fillable = [
+        'category_id','name'
+    ];
     public function categories()
     {
         return $this->belongsTo(Category::class);
