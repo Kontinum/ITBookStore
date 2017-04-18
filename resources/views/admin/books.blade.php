5e59baa3ca560286d1c0d0570ffa28dd7e3d33b1
Dejan <dejannrt411@gmail.com>
2017-04-18 22:58:53 +0200

    Adding CDN links for select2 js

--- resources/views/admin/books.blade.php
+++ resources/views/admin/books.blade.php
@@ -4,6 +4,11 @@
     Books panel
 @endsection
 
+@section('styles')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2">
