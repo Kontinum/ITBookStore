4e9079c5a22be5a2536d031d02489dfe9492b726
Dejan <dejannrt411@gmail.com>
2017-04-18 22:53:16 +0200

    Fetching authors and subcategories

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
 use App\Book;
+use App\Subcategory;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
@@ -10,7 +12,9 @@ class BookController extends Controller
     public function books()
     {
         $books = Book::orderBy('name','ASC')->get();
+        $authors = Author::all();
+        $subcategories = Subcategory::all();
 
-        return view('admin.books',['books' => $books]);
+        return view('admin.books',['books' => $books,'authors' => $authors,'subcategories' => $subcategories]);
     }
 }
