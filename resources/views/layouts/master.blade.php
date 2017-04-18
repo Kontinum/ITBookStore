a0936c1334974d26fc8d6b60c3d7aad3f22e59f1
Dejan <dejannrt411@gmail.com>
2017-04-18 23:17:28 +0200

    Replacing jquery CDN link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- Font Awesome CDN -->
     <script src="https://use.fontawesome.com/ddc4ec2a27.js"></script>
+    <!-- jQuery CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <!-- Custom styles -->
     <link rel="stylesheet" href="{{url()->to('css/styles.css')}}">
     @yield('styles')
@@ -21,8 +23,6 @@
         @include('errors.info-box')
         @yield('content')
     </div>
-    <!-- jQuery CDN -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <!-- Bootstrap JS CDN -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- Custom scripts -->
