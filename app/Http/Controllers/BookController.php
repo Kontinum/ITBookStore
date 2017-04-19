6712d192c6190a3ba4c0c8c6590017d8ce73f0f2
Dejan <dejannrt411@gmail.com>
2017-04-19 17:44:57 +0200

    Adding addBook and attachCategories methods

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Author;
 use App\Book;
+use App\Category;
+use App\Http\Requests\AddingBookRequest;
 use App\Subcategory;
 use Illuminate\Http\Request;
 
@@ -16,5 +18,34 @@ class BookController extends Controller
         $subcategories = Subcategory::all();
 
         return view('admin.books',['books' => $books,'authors' => $authors,'subcategories' => $subcategories]);
+    }
+
+    public function addBook(AddingBookRequest $request)
+    {
+        $book = new Book([
+            'isbn' => $request->input('book-isbn'),
+            'name' => $request->input('book-name'),
+            'year' => $request->input('book-year'),
+            'description' => $request->input('book-description'),
+            'price' => $request->input('book-price'),
+            'picture' => $request->input('book-picture'),
+            'pages' => $request->input('book-pages'),
+        ]);
+        $book->save();
+
+        $book->subcategories()->attach($request->input('book-categories'));
+        $this->attachCategories($book, $request->input('book-categories'));
+        $book->authors()->attach($request->input('book-authors'));
+
+        return back()->with('success','Book '.$book->name.' has been successfully added');
+    }
+
+    public function attachCategories(Book $book,$subcategoriesId)
+    {
+        foreach ($subcategoriesId as $subcategoryId) {
+            $subcategory = Subcategory::find($subcategoryId);
+            $category = Category::find($subcategory->category_id);
+            $book->categories()->attach($category->id);
+        }
     }
 }
