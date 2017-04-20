8e97fffd562ae7bf524a1d64627ec7fbcdaad302
Dejan <dejannrt411@gmail.com>
2017-04-20 16:59:36 +0200

    Adding code for form sliding

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,4 +10,22 @@ $('document').ready(function(){
     subcategoryToggle.click(function () {
         $(this).parent().next().slideToggle(700);
     });
+
+    //add book form toggle
+    var bookFormToggle = $('.addbook-toggle');
+    var bookForm = $('.addbook-form');
+
+    bookForm.hide();
+
+    bookFormToggle.click(function(){
+        bookForm.slideToggle(1000);
+
+        if(bookFormToggle.hasClass('fa-arrow-down')){
+            bookFormToggle.removeClass('fa-arrow-down');
+            bookFormToggle.addClass('fa-arrow-up');
+        }else{
+            bookFormToggle.removeClass('fa-arrow-up');
+            bookFormToggle.addClass('fa-arrow-down');
+        }
+    });
 });
