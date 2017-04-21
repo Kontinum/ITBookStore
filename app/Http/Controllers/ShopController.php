5f1bc3e62d08c4d51e326cd5accc81823a849e5a
Dejan <dejannrt411@gmail.com>
2017-04-22 01:19:21 +0200

    Fetching popularBooks and newBooks and passing them to view

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function getIndex()
     {
-        return view('shop.index');
+        $popularBooks = Book::orderBy('buyers','DESC')->limit(8)->get();
+        $newBooks = Book::orderBy('created_at','DESC')->limit(8)->get();
+        return view('shop.index',['popularBooks' => $popularBooks, 'newBooks' => $newBooks]);
     }
 }
