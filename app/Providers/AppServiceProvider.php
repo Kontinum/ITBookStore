662d0f8bc3be87ee898bb7fc9a2ae95e36eefe1e
Dejan <dejannrt411@gmail.com>
2017-04-21 14:53:09 +0200

    Sharing all categories to views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        View::share('categories',\App\Category::all());
     }
 
     /**
