2a590e591d43f1b4259555efb0c89d49e2881793
Dejan <dejannrt411@gmail.com>
2017-04-21 19:06:07 +0200

    Calling database seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(AuthorsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
     }
 }
