e45b418c2dac02947ad1f57ed93a113f19330f97
Dejan <dejannrt411@gmail.com>
2017-04-21 14:53:37 +0200

    Displaying categories and subcategories in navbar dropdown menu

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -20,13 +20,15 @@
                         <span class="caret"></span>
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
+                        @foreach($categories as $category)
+                            <li class="dropdown-menu-category"><a href="">{{$category->name}}</a>
+                                <ul class="dropdown-submenu">
+                                    @foreach($category->subcategories as $subcategory)
+                                        <li><a href="">{{$subcategory->name}}</a></li>
+                                    @endforeach
+                                </ul>
+                            </li>
+                        @endforeach
                     </ul>
                 </li>
             </ul>
