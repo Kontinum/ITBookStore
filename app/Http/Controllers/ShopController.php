69f09731af60a1fabd2d867abcb7137a75e0912e
Dejan <dejannrt411@gmail.com>
2017-04-23 00:04:40 +0200

    Adding book method

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use App\Category;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
@@ -12,5 +13,18 @@ class ShopController extends Controller
         $popularBooks = Book::orderBy('buyers','DESC')->limit(8)->get();
         $newBooks = Book::orderBy('created_at','DESC')->limit(8)->get();
         return view('shop.index',['popularBooks' => $popularBooks, 'newBooks' => $newBooks]);
+    }
+
+    public function book($bookId)
+    {
+        $book = Book::find($bookId);
+
+        if(!$book){
+            return redirect()->route('getIndex');
+        }
+
+        $categories = Category::all();
+
+        return view('shop.book', ['book' => $book, 'categories' => $categories]);
     }
 }
