451effffcbae6939a2997a17077d2458f57e0eb1
Dejan <dejannrt411@gmail.com>
2017-04-23 14:58:29 +0200

    Creating properties and constructor method

--- app/Cart.php
+++ app/Cart.php
@@ -4,5 +4,16 @@ namespace App;
 
 class Cart
 {
+    public $items = null,
+           $totalQty,
+           $totalPrice;
 
+    public function __construct($oldCart)
+    {
+        if($oldCart){
+            $this->items = $oldCart->items;
+            $this->totalQty = $oldCart->totalQty;
+            $this->totalPrice = $oldCart->totalPrice;
+        }
+    }
 }
