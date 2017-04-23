5d18e5c27a99d98427f894229fd9b7b93224283a
Dejan <dejannrt411@gmail.com>
2017-04-23 18:45:38 +0200

    Displaying number of items in a cart next to Shopping cart text

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -41,6 +41,9 @@
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">
                         <i class="fa fa-shopping-cart icon" aria-hidden="true"></i> Shopping cart
+                        @if(session()->has('cart'))
+                            <span class="badge">{{session()->get('cart')->totalQty}}</span>
+                        @endif
                     </a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
