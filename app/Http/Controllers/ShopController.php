b5ccb35bc339053bfa67ff52c3dc2fae3e570e11
Dejan <dejannrt411@gmail.com>
2017-04-23 18:00:41 +0200

    Adding addToCart method

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use App\Cart;
 use App\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ShopController extends Controller
 {
@@ -26,5 +28,23 @@ class ShopController extends Controller
         $categories = Category::all();
 
         return view('shop.book', ['book' => $book, 'categories' => $categories]);
+    }
+
+    public function addToCart($bookId)
+    {
+        $book = Book::find($bookId);
+
+        if(!$book){
+            return redirect()->route('getIndex');
+        }
+
+        $oldCart = session()->has('cart') ? session()->get('cart') : null;
+
+        $cart = new Cart($oldCart);
+        $cart->add($book);
+
+        session()->put('cart',$cart);
+
+        return back();
     }
 }
