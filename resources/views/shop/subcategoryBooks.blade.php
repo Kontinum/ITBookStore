de4ccce848caf1ade0cd4400548b876847320b5d
Dejan <dejannrt411@gmail.com>
2017-04-26 21:53:39 +0200

    Adding css class to category and subcategory name

--- resources/views/shop/subcategoryBooks.blade.php
+++ resources/views/shop/subcategoryBooks.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="row">
         <div class="col-lg-9 col-lg-offset-1">
-            <h2><i class="fa fa-book icon" aria-hidden="true"></i> <em>{{$subcategoryName}}</em> books</h2>
+            <h2><i class="fa fa-book icon" aria-hidden="true"></i> <em class="search-count">{{$subcategoryName}}</em> books</h2>
             <hr style="background-color: cornflowerblue;margin-top: 0" class="col-lg-12">
         </div>
         @include('partials.bookList')
