9d416775834c1df95828dd8ae557b8fc5566e034
Dejan <dejannrt411@gmail.com>
2017-04-26 19:50:48 +0200

    Creating bookList partial

---
+++ resources/views/partials/bookList.blade.php
@@ -0,0 +1,26 @@
+@foreach($books as $book)
+    <div class="col-lg-9 col-lg-offset-1">
+        <div class="row">
+            <div class="media">
+                <div class="media-left col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <a href="{{route('book',['bookId' => $book->id])}}">
+                        <img class="media-object img-responsive" src="{{$book->picture}}" alt="{{$book->name}} picture">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <a style="text-decoration: none" href="{{route('book',['bookId' => $book->id])}}"><h4 class="media-heading"><em style="font-weight: bold">{{$book->name}}</em></h4></a>
+                    By:@foreach($book->authors as $author)
+                        <a href="">{{$author->name}}</a>
+                        @if($loop->remaining)
+                            , &nbsp;
+                        @endif
+                    @endforeach<br><br>
+                    <span class="">{{substr($book->description,0,500)}}...</span>
+                    <br><br>
+                    Price:<em class="book-price">{{$book->price}}$</em>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+@endforeach
