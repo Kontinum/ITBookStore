3f0c9b4f8afd0c8f8be251a29846344e5b1b4796
Dejan <dejannrt411@gmail.com>
2017-04-26 20:02:44 +0200

    Creating subcategoryBooks view

---
+++ resources/views/shop/subcategoryBooks.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('title')
+    {{$subcategoryName}} books
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-9 col-lg-offset-1">
+            <h2><i class="fa fa-book icon" aria-hidden="true"></i> <em>{{$subcategoryName}}</em> books</h2>
+            <hr style="background-color: cornflowerblue;margin-top: 0" class="col-lg-12">
+        </div>
+        @include('partials.bookList')
+    </div>
+@endsection
