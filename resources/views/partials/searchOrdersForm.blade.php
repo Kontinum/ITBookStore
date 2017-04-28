963c6e746654519e8af06a427128e7bc5845cf78
Dejan <dejannrt411@gmail.com>
2017-04-28 23:09:28 +0200

    Creating searchOrdersForm partial

---
+++ resources/views/partials/searchOrdersForm.blade.php
@@ -0,0 +1,16 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        Search orders
+    </div>
+    <div class="panel-body">
+        <form action="{{route('addAuthor')}}" method="post">
+            {{csrf_field()}}
+            <div class="form-group">
+                <input type="text" name="order-id" value="{{old('order-id')}}" class="form-control" placeholder="Order payment ID" required>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary pull-right">Search</button>
+            </div>
+        </form>
+    </div>
+</div>
