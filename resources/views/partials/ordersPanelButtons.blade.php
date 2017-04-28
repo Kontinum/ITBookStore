e9ff99ef14ddca6e01f168eb27124383ccb5f751
Dejan <dejannrt411@gmail.com>
2017-04-29 00:17:16 +0200

    Creating ordersPanelButtons partial

---
+++ resources/views/partials/ordersPanelButtons.blade.php
@@ -0,0 +1,11 @@
+<div class="text-center">
+    <a style="text-decoration: none" href="{{route('uncheckedOrders')}}">
+        <button class="btn btn-primary">Unchecked orders</button>
+    </a>
+    <a style="text-decoration: none" href="">
+        <button class="btn btn-primary">Checked orders</button>
+    </a>
+    <a href="">
+        <button class="btn btn-primary">Delivered orders</button>
+    </a>
+</div>
