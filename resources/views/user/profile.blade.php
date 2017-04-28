b97a03e7d40d9f530b1fd8bf14035d4a9f88ccaa
Dejan <dejannrt411@gmail.com>
2017-04-28 14:00:07 +0200

    Adding some links to a profile page and linking getChangePassword route

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -10,4 +10,23 @@
             <p><i class="search-count">{{auth()->user()->username}}</i> profile</p>
         </div>
     </div>
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <a style="text-decoration: none" href="">
+                <button class="btn btn-primary">
+                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>    Edit profile
+                </button>
+            </a>
+            <a style="text-decoration: none" href="{{route('getChangePassword')}}">
+                <button class="btn btn-primary">
+                    <i class="fa fa-key" aria-hidden="true"></i> Change password
+                </button>
+            </a>
+            <a href="">
+                <button class="btn btn-primary">
+                    <i class="fa fa-list" aria-hidden="true"></i> View orders
+                </button>
+            </a>
+        </div>
+    </div>
 @endsection
